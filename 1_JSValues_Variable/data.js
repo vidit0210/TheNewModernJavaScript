d949d3e2708516400c6763698ff6c991f74651a4
vidit0210 <vidit0210@gmail.com>
2020-01-04 03:37:07 +0530

    Create data.js
    section2

---
+++ 1_JSValues_Variable/data.js
@@ -0,0 +1,11 @@
+// Numbers ..
+console.log(2 ** 3);
+let name;
+let document = 15;
+//const
+
+let name = 'Vidit';
+let isAngry = false;
+// Strings are immutable.........
+let indexing = name.toUpperCase();
+let templateString = ` hey this is a template string from ${name}`;
